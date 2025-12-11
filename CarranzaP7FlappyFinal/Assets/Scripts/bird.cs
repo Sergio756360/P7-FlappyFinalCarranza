56e01cbf1823c2cd2a13b638bd978729a8ee9e57
Sergio756360 <756360@student.dusd.net>
2025-12-11 15:27:35 -0800

    Animations and Game Over
    Started adding all the animations and transitions including the Game Over text

--- CarranzaP7FlappyFinal/Assets/Scripts/bird.cs
+++ CarranzaP7FlappyFinal/Assets/Scripts/bird.cs
@@ -9,11 +9,13 @@ public class bird : MonoBehaviour
 
     private bool IsDead = false;
     private Rigidbody2D rb2d;
+    private Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -25,6 +27,7 @@ public class bird : MonoBehaviour
             {
                 rb2d.velocity = Vector2.zero;
                 rb2d.AddForce(new Vector2(0, upforce));
+                anim.SetTrigger("Flap");
             }
         }
     }
@@ -33,6 +36,7 @@ public class bird : MonoBehaviour
     void OnCollisionEnter2D()
     {
         IsDead = true;
+        anim.SetTrigger("Die");
     }
 }
 
